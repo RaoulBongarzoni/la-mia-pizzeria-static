2074a6ca857247db161edc8b63cf5756ce1eb6ce
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-05-14 16:14:03 +0200

    Aggiungere i file di progetto.

---
+++ Models/PizzaModel.cs
@@ -0,0 +1,23 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaModel
+    {
+        public string PizzaName { get; set; }
+
+        public string PizzaDescription { get; set; }    
+        public string PizzaPhoto { get; set; }
+
+        public decimal PizzaPrice {  get; set; }
+
+
+        public PizzaModel() { }
+
+        public PizzaModel(string name , string description, string photo, decimal price)
+        {
+            this.PizzaName = name;
+                this.PizzaDescription = description;
+            this.PizzaPhoto = photo;
+            this.PizzaPrice = price;
+        }
+    }
+}
